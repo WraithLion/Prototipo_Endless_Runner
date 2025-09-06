86c92874dc21c108288442b86adb6b05de9f5e8b
AlanE <alanescalante7@gmail.com>
2025-09-05 18:10:10 -0600

    UI y menu principal, placeholders

---
+++ Protipo_Endless_Runner/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public string juego;
+    public string inicio;
+    public void Restart(string scenename)
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(scenename);
+
+    }
+    public void Menu()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(inicio);
+    }
+}
