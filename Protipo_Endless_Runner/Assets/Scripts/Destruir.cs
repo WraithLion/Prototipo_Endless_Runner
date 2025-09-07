3dcf5be4f21522a64c7dfd1dbad21d72e516c5e6
AlanE <alanescalante7@gmail.com>
2025-09-07 11:44:50 -0600

    Funcionalidad de disparo
    El disparo ya destruye los obstaculos, cada obstaculo tiene un nivel de vida customizable

--- Protipo_Endless_Runner/Assets/Scripts/Destruir.cs
+++ Protipo_Endless_Runner/Assets/Scripts/Destruir.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Destruir : MonoBehaviour
 {
-    void onTriggerEnter(Collider other)
+    public int vida;
+    void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Disparo"))
         {
-            Destroy(gameObject);
+            Destroy(other.gameObject);
+            vida--;
+            if (vida <= 0)
+            {
+                Destroy(this.gameObject);
+            }
         }
     }
 }
