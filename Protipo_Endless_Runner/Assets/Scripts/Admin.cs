42e498d5c8b4d078527f6532deac5d7b76ead28e
AlanE <alanescalante7@gmail.com>
2025-09-11 07:33:26 -0600

    dificultad inicial

--- Protipo_Endless_Runner/Assets/Scripts/Admin.cs
+++ Protipo_Endless_Runner/Assets/Scripts/Admin.cs
@@ -1,22 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class Admin : MonoBehaviour
 {
     public int dificultadGlobal;
-    public TextMeshProUGUI textoDificultad;
 
     private void Start()
     {
         StartCoroutine(SubirDificultad());
     }
 
-    void Update()
-    {
-        textoDificultad.text = dificultadGlobal.ToString();
-    }
+    
+    
     private IEnumerator SubirDificultad()
     {
         while (true)
